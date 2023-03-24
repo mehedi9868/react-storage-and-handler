a47de4cf691c834a159acbd4a5f7e4d2857ab502
mehedi9868 <mehedi9868@gmail.com>
2023-03-25 02:45:45 +0600

    generate data using json generator

--- src/components/Cosmetic/Cosmetic.js
+++ src/components/Cosmetic/Cosmetic.js
@@ -2,11 +2,12 @@ import React from 'react';
 
 const Cosmetic = (props) => {
     const { cosmetic } = props;
-    const { name, price } = cosmetic;
+    const { name, price, id } = cosmetic;
     return (
         <div>
             <h2>Buy This:{name}</h2>
             <p>Price:${price}</p>
+            <p><small>ID:{id}</small></p>
         </div>
     );
 };
--- src/components/Cosmetics/Cosmetics.js
+++ src/components/Cosmetics/Cosmetics.js
@@ -1,14 +1,13 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Cosmetic from '../Cosmetic/Cosmetic';
 import './Cosmetics.css';
 const Cosmetics = () => {
-    const cosmetics = [
-        { id: 1, name: 'Alta', price: 100 },
-        { id: 2, name: 'Alta', price: 200 },
-        { id: 3, name: 'Alta', price: 300 },
-        { id: 4, name: 'Alta', price: 400 },
-        { id: 5, name: 'Alta', price: 500 }
-    ];
+    const [cosmetics, setCosmetics] = useState([]);
+    useEffect(() => {
+        fetch('generated.json')
+            .then(res => res.json())
+            .then(data => setCosmetics(data));
+    }, [])
     return (
         <div>
             <h2>Welcome to my cosmetics store !!!</h2>
