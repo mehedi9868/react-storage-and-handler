63bdd6550a6c4d625eca5478763b6555b4dd201d
mehedi9868 <mehedi9868@gmail.com>
2023-03-25 03:07:24 +0600

    handler with parameter

--- src/components/Cosmetic/Cosmetic.js
+++ src/components/Cosmetic/Cosmetic.js
@@ -1,13 +1,17 @@
 import React from 'react';
-
+import './Cosmetic.css';
 const Cosmetic = (props) => {
     const { cosmetic } = props;
     const { name, price, id } = cosmetic;
+    const handleCart = (id) => {
+        console.log('product added:', id);
+    } // use wrapper '()=>' to onClick to pass parameters. 
     return (
-        <div>
+        <div className='product'>
             <h2>Buy This:{name}</h2>
             <p>Price:${price}</p>
             <p><small>ID:{id}</small></p>
+            <button onClick={() => handleCart(id)}>Add to Cart</button>
         </div>
     );
 };
